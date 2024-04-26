e1cc34dfc207767ed2e2fa6856e9a3d644323cd7
Senko <68884768+ItsSenko@users.noreply.github.com>
2024-04-26 05:49:26 -0400

    Project Commit

---
+++ WebsocketMediaControl/MediaControl.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Timer = System.Timers.Timer;
+
+namespace MediaControl
+{
+    internal class MediaControl
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        private static readonly int KEYEVENTF_KEYUP = 0x0002;
+
+        private const byte MediaPlayPause = (byte)Keys.MediaPlayPause;
+        private const byte MediaNextTrack = (byte)Keys.MediaNextTrack;
+        private const byte MediaPreviousTrack = (byte)Keys.MediaPreviousTrack;
+        private const byte MediaStop = (byte)Keys.MediaStop;
+
+        public static void PlayPause()
+        {
+            keybd_event(MediaPlayPause, MediaPlayPause, 0, 0);
+            keybd_event(MediaPlayPause, MediaPlayPause, KEYEVENTF_KEYUP, 0);
+        }
+        public static void PrevTrack()
+        {
+            keybd_event(MediaPreviousTrack, MediaPreviousTrack, 0, 0);
+            keybd_event(MediaPreviousTrack, MediaPreviousTrack, KEYEVENTF_KEYUP, 0);
+        }
+        public static void NextTrack()
+        {
+            keybd_event(MediaNextTrack, MediaNextTrack, 0, 0);
+            keybd_event(MediaNextTrack, MediaNextTrack, KEYEVENTF_KEYUP, 0);
+        }
+        public static void Stop()
+        {
+            keybd_event(MediaStop, MediaStop, 0, 0);
+            keybd_event(MediaStop, MediaStop, KEYEVENTF_KEYUP, 0);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindow(IntPtr hWnd);
+
+        private IntPtr _spotifyWindow;
+        private string _currentSong;
+
+        public string CurrentSong
+        {
+            get { return _currentSong; }
+            set 
+            {
+                if (_currentSong != value)
+                {
+                    _currentSong = value;
+                    OnSongChanged?.Invoke(value);
+                }
+            }
+        }
+
+        private bool _connectedToSpotify;
+
+        public bool ConnectedToSpotify
+        {
+            get { return _connectedToSpotify; }
+            set 
+            { 
+                if (_connectedToSpotify != value)
+                {
+                    _connectedToSpotify = value;
+                }
+            }
+        }
+
+
+        public delegate void OnSongChangedHandler(string newSong);
+        public OnSongChangedHandler OnSongChanged;
+
+        public delegate void OnConnectionChangedHandler(bool connected);
+        public OnConnectionChangedHandler OnConnectionChanged;
+
+
+        public void Initialize()
+        {
+            Timer timer = new(0.1f);
+            timer.Elapsed += Update;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private void Update(object? sender, ElapsedEventArgs e)
+        {
+            ConnectedToSpotify = _spotifyWindow != IntPtr.Zero && IsWindow(_spotifyWindow);
+
+            if (_spotifyWindow == IntPtr.Zero || !IsWindow(_spotifyWindow))
+            {
+                var spotifyProcess = Process.GetProcessesByName("Spotify").FirstOrDefault();
+                if (spotifyProcess == null)
+                    return;
+
+                _spotifyWindow = new MainWindowFinder().FindMainWindow(spotifyProcess.Id);
+                if (_spotifyWindow == IntPtr.Zero || !IsWindow(_spotifyWindow))
+                    return;
+            }
+
+            var len = GetWindowTextLength(_spotifyWindow) * 2;
+            var sb = new StringBuilder(len + 1);
+            GetWindowText(_spotifyWindow, sb, sb.Capacity);
+
+            var song = sb.ToString();
+            CurrentSong = !song.StartsWith("Spotify") ? song : string.Empty;
+        }
+    }
+
+    internal class MainWindowFinder
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr GetWindow(IntPtr hWnd, GetWindowType uCmd);
+        private delegate bool CallBackPtr(IntPtr hwnd, int lParam);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool EnumWindows(CallBackPtr lpEnumFunc, int lParam);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        private enum GetWindowType : uint
+        {
+            GW_HWNDFIRST = 0,
+            GW_HWNDLAST = 1,
+            GW_HWNDNEXT = 2,
+            GW_HWNDPREV = 3,
+            GW_OWNER = 4,
+            GW_CHILD = 5,
+            GW_ENABLEDPOPUP = 6
+        }
+
+        private IntPtr _bestHandle;
+        private int _processId;
+
+        public IntPtr FindMainWindow(int processId)
+        {
+            _bestHandle = IntPtr.Zero;
+            _processId = processId;
+
+            EnumWindows(EnumWindowsThunk, _processId);
+
+            return _bestHandle;
+        }
+
+        private bool EnumWindowsThunk(IntPtr hWnd, int processId)
+        {
+            GetWindowThreadProcessId(hWnd, out var pid);
+            if (pid != processId || !IsMainWindow(hWnd))
+                return true;
+            _bestHandle = hWnd;
+            return false;
+        }
+
+        private static bool IsMainWindow(IntPtr hWnd)
+        {
+            if (GetWindow(hWnd, GetWindowType.GW_OWNER) == IntPtr.Zero && IsWindowVisible(hWnd))
+                return true;
+            return false;
+        }
+    }
+}
---
+++ WebsocketMediaControl/Program.cs
@@ -0,0 +1,110 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace MediaControl
+{
+    public class WebsocketMediaControl
+    {
+        public static string path;
+        public static void Main(string[] args)
+        {
+            path = Path.Combine(Directory.GetCurrentDirectory(), "Settings.txt");
+            if (!File.Exists(path))
+            {
+                Logger.Log("Settings file does not exist, creating one...");
+
+                Logger.Log("Please input the url for the websocket server. If left empty it will default to 'ws://localhost:3000'");
+                string urlInput = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(urlInput))
+                {
+                    urlInput = "ws://localhost:3000";
+                }
+
+                File.WriteAllText(path, urlInput);
+                Logger.Log("Settings Created");
+            }
+
+            string url = File.ReadAllText(path);
+
+            Logger.Log("Starting Server");
+            WebSocketServer server = new(url);
+            server.AddWebSocketService<Server>("/");
+            server.Start();
+
+            Logger.Log($"Server started at {url}/");
+            while (Console.ReadKey().Key != ConsoleKey.Q) { }
+        }
+    }
+    public class Server : WebSocketBehavior
+    {
+        private MediaControl media;
+        private bool isConnectionOpen;
+        protected override void OnOpen()
+        {
+            Logger.Log("Client Connected");
+            media = new MediaControl();
+            media.Initialize();
+            media.OnSongChanged += OnSongChanged;
+            isConnectionOpen = true;
+        }
+        protected override void OnClose(CloseEventArgs e)
+        {
+            Logger.Log("Client Disconnected");
+            isConnectionOpen = false;
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Logger.Log($"Recieved {e.Data}");
+            switch (e.Data)
+            {
+                case "next":
+                    {
+                        MediaControl.NextTrack();
+                        Logger.Log($"Skipped to Next Song");
+                        Send("log:Skipped to Next Song");
+                        break;
+                    }
+                case "play":
+                    {
+                        MediaControl.PlayPause();
+                        Logger.Log("Toggled Playing");
+                        Send("log:Toggled Play Mode");
+                        break;
+                    }
+                case "prev":
+                    {
+                        MediaControl.PrevTrack();
+                        Logger.Log("Returned to Previous Track");
+                        Send("log:Returned to Previous Track");
+                        break;
+                    }
+            }
+        }
+
+        protected override void OnError(WebSocketSharp.ErrorEventArgs e)
+        {
+            Logger.Log($"ERROR: {e.Message}");
+        }
+
+        private void OnSongChanged(string newSong)
+        {
+            if (isConnectionOpen)
+                Send($"song:{newSong}");
+            if (!string.IsNullOrEmpty(newSong) && isConnectionOpen)
+                Send($"log:Playing {newSong}");
+
+            Logger.Log($"Song Changed to {newSong}");
+        }
+    }
+
+    public class Logger
+    {
+        public static void Log(string message)
+        {
+            var time = DateTime.Now.ToString("[hh:mm:ss]");
+            Console.WriteLine($"{time}: {message}");
+        }
+    }
+}
